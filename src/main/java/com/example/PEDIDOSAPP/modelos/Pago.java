c083c595b3c9b15ca1bb4f1b8c5dccc1bd2849dc
yomapg <yoma0639@gmail.com>
2025-02-27 13:01:50 -0500

    se empieza con las anotaciones de jpa

--- src/main/java/com/example/PEDIDOSAPP/modelos/Pago.java
+++ src/main/java/com/example/PEDIDOSAPP/modelos/Pago.java
@@ -11,4 +11,55 @@ public class Pago {
     private PagoEnum metodoPago;
     private PagoEstadoEnum estado;
     private LocalDate fechaPago;
+
+    public Pago() {
+    }
+
+    public Pago(Integer id, Integer idPedido, PagoEnum metodoPago, PagoEstadoEnum estado, LocalDate fechaPago) {
+        this.id = id;
+        this.idPedido = idPedido;
+        this.metodoPago = metodoPago;
+        this.estado = estado;
+        this.fechaPago = fechaPago;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(Integer idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public PagoEnum getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(PagoEnum metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+
+    public PagoEstadoEnum getEstado() {
+        return estado;
+    }
+
+    public void setEstado(PagoEstadoEnum estado) {
+        this.estado = estado;
+    }
+
+    public LocalDate getFechaPago() {
+        return fechaPago;
+    }
+
+    public void setFechaPago(LocalDate fechaPago) {
+        this.fechaPago = fechaPago;
+    }
 }
--- src/main/java/com/example/PEDIDOSAPP/modelos/Usuario.java
+++ src/main/java/com/example/PEDIDOSAPP/modelos/Usuario.java
@@ -1,10 +1,19 @@
 package com.example.PEDIDOSAPP.modelos;
 
 import com.example.PEDIDOSAPP.ayudas.enums.UsuarioEnum;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name="usuario_tabla")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_usuario")
     private  Integer id;
+    @Column(name = "nombre",length = 100,unique = true,nullable = false)
     private  String nombre;
+    @Column(name="email",length = 150,unique = true,nullable = false)
     private String correoElectronico;
     private String contraseña;
     private String telefono;
